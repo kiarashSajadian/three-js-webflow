db0082eb266ffe607857fb8e14b015699efdfd26
Kiarash Sajadian <kiarashsajadian@gmail.com>
2025-02-27 06:52:05 +0100

    adding camera

--- src/main.js
+++ src/main.js
@@ -5,3 +5,9 @@ console.log("Hello from Kiarash Sajadiann");
 console.log(THREE);
 
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
